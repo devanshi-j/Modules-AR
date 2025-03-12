b5b5e83f90a7d4b33a7624ccd532567404397768
Devanshi Jhawar <89416589+devanshi-j@users.noreply.github.com>
2025-03-12 09:39:44 +0530

    Create modelLibrary.js

---
+++ scripts/modelLibrary.js
@@ -0,0 +1,37 @@
+export const itemCategories = {
+    chair: [
+        { name: "chair4", height: 0.5 },
+        { name: "chair2", height: 0.5 },
+        { name: "chair3", height: 0.5 },
+        { name: "chair2", height: 0.5 },
+        { name: "chair2", height: 0.5 }
+    ],
+    sofa: [
+        { name: "sofa3", height: 0.5 },
+        { name: "sofa2", height: 0.5 },
+        { name: "sofa3", height: 0.5 },
+        { name: "sofa2", height: 0.5 },
+        { name: "sofa2", height: 0.5 }
+    ],
+    table: [
+        { name: "table2", height: 0.5 },
+        { name: "table2", height: 0.5 },
+        { name: "table3", height: 0.5 },
+        { name: "table4", height: 0.5 },
+        { name: "table5", height: 0.5 }
+    ],
+    vase: [
+        { name: "vase1", height: 0.5 },
+        { name: "vase2", height: 0.5 },
+        { name: "vase3", height: 0.5 },
+        { name: "vase4", height: 0.5 },
+        { name: "vase5", height: 0.5 }
+    ],
+    rug: [
+        { name: "rug1", height: 0.5 },
+        { name: "rug2", height: 0.5 },
+        { name: "rug3", height: 0.5 },
+        { name: "rug4", height: 0.5 },
+        { name: "rug5", height: 0.5 }
+    ],
+};
